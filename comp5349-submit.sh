e104376497d9191c8205035fc61562d187f056dd
PatrickTian-USYD <30972140+PatrickTian-USYD@users.noreply.github.com>
2022-05-25 11:46:13 +1000

    Update comp5349-submit.sh

--- comp5349-submit.sh
+++ comp5349-submit.sh
@@ -1,6 +1,6 @@
-spark-submit \
+spark-submit\
     --master yarn\
-    --deploy-mode client \
-    --num-executors 3 \
-    Comp5349-A2.py \
+    --deploy-mode client\
+    --num-executors 3\
+    Comp5349-A2.py\
     --output $1
